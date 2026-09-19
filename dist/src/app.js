6b3c3f012291b12f47a1b7b7670dd004d03ca3b7
Uju Chinedum <ujuchimaraoke@gmail.com>
2026-09-19 12:11:59 +0100

    feat: setup project

---
+++ dist/src/app.js
@@ -0,0 +1,3 @@
+"use strict";
+const serverMessage = "Hello, TypeScript with Node.js!";
+console.log(serverMessage);
---
+++ test/app.test.ts
@@ -0,0 +1,5 @@
+describe("Initial Smoke Test", () => {
+  it("should assert that true is equal to true", () => {
+    expect(true).toBe(true);
+  });
+});
